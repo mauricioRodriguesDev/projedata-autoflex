c99c74cdadb54cf3029134d1342f535e1c2c6132
Mauricio Rodrigues <mauriciorodrigues.dev94@gmail.com>
2026-03-03 20:01:43 -0300

    chore: finish backend implementation

--- backend/src/main/java/com/projedata/autoflex/infrastructure/repository/ProductRepository.java
+++ backend/src/main/java/com/projedata/autoflex/infrastructure/repository/ProductRepository.java
@@ -10,11 +10,6 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
-    /**
-     * Fetches all products with their compositions and associated raw materials in a single query
-     * to avoid the N+1 problem. The results are ordered by price in descending order.
-     * @return A list of products, optimized for the production suggestion calculation.
-     */
     @Query("SELECT p FROM Product p LEFT JOIN FETCH p.composition c LEFT JOIN FETCH c.rawMaterial ORDER BY p.price DESC")
     List<Product> findAllWithCompositionAndRawMaterialOrderByPriceDesc();
 }
